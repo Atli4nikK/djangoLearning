caee287de5552ec7124ab1415dc3940bcdb9742c
Atli4nikK <iliyaanderson1337@gmail.com>
2018-12-04 23:48:46 +0300

    Data Transfer from Python to HTML(Jinja)

--- mainApp/views.py
+++ mainApp/views.py
@@ -5,3 +5,8 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'mainApp/homePage.html')
+
+
+def contact(request):
+    return render(request, 'mainApp/basic.html',
+                  {'values': ['Контактный телефон', '(918) 123-45-67', 'example@gmail.com']})
