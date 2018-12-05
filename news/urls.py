6f01dce69d11c03117a2be68a529f801ed79e56b
Atli4nikK <iliyaanderson1337@gmail.com>
2018-12-05 16:59:27 +0300

    Migrations and admin panel

--- news/urls.py
+++ news/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from django.views.generic import ListView, DetailView
-from mysite.news.models import Articles
+from news.models import Articles
 
 urlpatterns = [
     path('', ListView.as_view(queryset=Articles.objects.all().order_by("-date")[:20],
